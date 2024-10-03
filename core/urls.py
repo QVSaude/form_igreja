1534aa1b890abc100a6cb7e49dfc79c1c3995e0a
AndersonMoura <andersonmoura812@gmail.com>
2024-10-03 09:44:40 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('AESP_odonto.urls')),
-    path('empresa/', include('empresas_AESP_odonto.urls')),
+    path('igreja', include('AESP_odonto.urls')),
+    path('aesp/', include('empresas_AESP_odonto.urls')),
 ]
