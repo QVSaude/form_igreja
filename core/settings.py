69552e064bc967913aec178c91b9e28ebae0a0ef
AndersonMoura <andersonmoura812@gmail.com>
2025-05-14 11:42:28 -0300

    up

--- core/settings.py
+++ core/settings.py
@@ -111,14 +111,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
